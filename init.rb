a2c4abf916a98ced35cb7c77d19494509a5b54c9
benl96 <b.lienert@ablida.de>
2024-02-13 09:48:41 +0100

    change url, update description, initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :remove_downcase_for_button_save_object do
+  name 'Remove Downcase For Button Save Object plugin'
+  author 'glauni'
+  description 'Removes downcase for button_save_object in order to provide accurate german translations'
+  version '0.0.2'
+  url 'https://github.com/ablidadev/remove_downcase_for_button_save_object'
+  author_url ''
+end
