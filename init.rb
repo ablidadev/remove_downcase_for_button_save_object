fa3f9c8c665799c1bd6595c59349294b5a844932
ablidadev <126158215+ablidadev@users.noreply.github.com>
2024-02-13 10:03:20 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :remove_downcase_for_button_save_object do
   name 'Remove Downcase For Button Save Object plugin'
-  author 'glauni'
+  author 'ablidadev'
   description 'Removes downcase for button_save_object in order to provide accurate german translations'
   version '0.0.2'
   url 'https://github.com/ablidadev/remove_downcase_for_button_save_object'
